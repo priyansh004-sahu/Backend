539bb2c04e5fea0af41b655c9b2e0a0b15da43fd
priyansh sahu <sahupriyansh298@gmail.com>
2025-08-05 23:06:41 +0530

    auth

--- Day_138/src/controllers/auth.controller.js
+++ Day_138/src/controllers/auth.controller.js
@@ -31,12 +31,30 @@ async function registerController(req, res) {
 }
 
 async function loginController(req, res) {
+    const { username, password } = req.body;
 
-    const {username, password} = req.body;
+    const user = await userModel.findOne({
+        username
+    })
 
-    
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
+    }
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.cookie("token", token);
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            username: user.username,
+            id: user._id
+        }
+    })
 }
 
 module.exports ={
