76eb43ec527bd50f05b346b3aeca9d63d3c61100
priyansh sahu <sahupriyansh298@gmail.com>
2025-09-10 19:06:33 +0530

    .

--- pwa/src/main.jsx
+++ pwa/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+// PWA
 import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 
